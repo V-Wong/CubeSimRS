b55599d16d92c9f1d4198dbfdcbbadd829ca2dee
Vincent Wong <vincent@vwong.dev>
2021-08-09 19:22:33 +1000

    Replace dynamic polymorphism with static polymorphism

--- src/generic_cube/cube.rs
+++ src/generic_cube/cube.rs
@@ -1,7 +1,7 @@
 pub trait Cube {
     fn is_solved(&self) -> bool;
     fn get_state(&self) -> Vec<Face>;
-    fn apply_move(&self, mv: Move) -> Box<dyn Cube>;
+    fn apply_move(&self, mv: Move) -> Self;
 }
 
 #[derive(Debug)]
