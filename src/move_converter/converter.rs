22a07e40a94a2f370fc27847c44db12177ea0780
Vincent Wong <vincent@vwong.dev>
2021-08-11 23:45:34 +1000

    Remove hard coded pattern matching of move variants

--- src/move_converter/converter.rs
+++ src/move_converter/converter.rs
@@ -1,4 +1,4 @@
-use crate::generic_cube::{Move};
+use crate::generic_cube::{Move, MoveVariant};
 use crate::generic_cube::Move::*;
 use crate::generic_cube::MoveVariant::*;
 
@@ -7,34 +7,30 @@ pub fn from_scramble_string(scramble: String) -> Vec<Move> {
 }
 
 fn convert_move(mv: &str) -> Move {
-    match mv {
-        "U" => U(Standard),
-        "R" => R(Standard),
-        "F" => F(Standard),
-        "L" => L(Standard),
-        "D" => D(Standard),
-        "B" => B(Standard),
-        "X" => X(Standard),
-        "Y" => Y(Standard),
-        "Z" => Z(Standard),
-        "U'" => U(Inverse),
-        "R'" => R(Inverse),
-        "F'" => F(Inverse),
-        "L'" => L(Inverse),
-        "D'" => D(Inverse),
-        "B'" => B(Inverse),
-        "X'" => X(Inverse),
-        "Y'" => Y(Inverse),
-        "Z'" => Z(Inverse),
-        "U2" => U(Double),
-        "R2" => R(Double),
-        "F2" => F(Double),
-        "L2" => L(Double),
-        "D2" => D(Double),
-        "B2" => B(Double),
-        "X2" => X(Double),
-        "Y2" => Y(Double),
-        "Z2" => Z(Double),
+    let variant = get_variant(mv);
+
+    match &mv[0..1] {
+        "U" => U(variant),
+        "R" => R(variant),
+        "F" => F(variant),
+        "L" => L(variant),
+        "D" => D(variant),
+        "B" => B(variant),
+        "X" => X(variant),
+        "Y" => Y(variant),
+        "Z" => Z(variant),
         _ => panic!()
     }
+}
+
+fn get_variant(mv: &str) -> MoveVariant {
+    if mv.contains("2") {
+        return Double
+    }
+
+    if mv.contains("'") {
+        return Inverse
+    }
+
+    Standard
 }
