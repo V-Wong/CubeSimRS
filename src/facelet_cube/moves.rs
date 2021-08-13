9db45b18d3737434e1b52117c74d0b627af760e3
Vincent Wong <vincent@vwong.dev>
2021-08-14 02:30:43 +1000

    Memoize geometric to facelet move conversions

--- src/facelet_cube/moves.rs
+++ src/facelet_cube/moves.rs
@@ -1,10 +1,28 @@
 use std::collections::HashMap;
 use cgmath::Vector3;
+use lazy_static::lazy_static;
 
-use crate::generic_cube::{Cube, Move, MoveVariant};
+use crate::generic_cube::{Cube, Move};
+use crate::generic_cube::Move::*;
+use crate::generic_cube::MoveVariant::*;
 use crate::geometric_cube::{GeoCube, Sticker, cube3};
 
-pub fn from_geometric_move(mv: Move) -> Vec<(i32, i32)> {
+#[derive(Clone)]
+pub struct FaceletMove(pub Vec<(i32, i32)>);
+
+pub fn convert_move(mv: Move) -> &'static FaceletMove {
+    match mv {
+        U(variant) => &U_MOVE,
+        R(variant) => &R_MOVE,
+        F(variant) => &F_MOVE,
+        L(variant) => &L_MOVE,
+        D(variant) => &D_MOVE,
+        B(variant) => &B_MOVE,
+        _ => &F_MOVE
+    }
+}
+
+fn from_geometric_move(mv: Move) -> Vec<(i32, i32)> {
     let index_map: HashMap<Vector3<i32>, i32> = create_index_conversion_map();
 
     cube3().apply_move(mv).0
@@ -20,9 +38,6 @@ pub fn from_geometric_move(mv: Move) -> Vec<(i32, i32)> {
 }
 
 fn create_index_conversion_map() -> HashMap<Vector3<i32>, i32>{
-    use Move::*;
-    use MoveVariant::*;
-
     let mut map = HashMap::new();
 
     let face_rotating_moves = vec![
@@ -46,4 +61,13 @@ fn create_index_conversion_map() -> HashMap<Vector3<i32>, i32>{
     }
 
     map
+}
+
+lazy_static! {
+    pub static ref U_MOVE: FaceletMove = FaceletMove(from_geometric_move(U(Standard)));
+    pub static ref R_MOVE: FaceletMove = FaceletMove(from_geometric_move(R(Standard)));
+    pub static ref F_MOVE: FaceletMove = FaceletMove(from_geometric_move(F(Standard)));
+    pub static ref D_MOVE: FaceletMove = FaceletMove(from_geometric_move(D(Standard)));
+    pub static ref L_MOVE: FaceletMove = FaceletMove(from_geometric_move(L(Standard)));
+    pub static ref B_MOVE: FaceletMove = FaceletMove(from_geometric_move(B(Standard)));
 }
