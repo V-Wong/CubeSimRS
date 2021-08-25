cab649ffad5fc2a36151b278afcd2ee6070f75e5
Vincent Wong <vincent@vwong.dev>
2021-08-25 13:26:31 +1000

    Add size parameter to tests and benchmarks

--- benches/geometric_cube.rs
+++ benches/geometric_cube.rs
@@ -8,19 +8,19 @@ use cubesim::{Cube, Move, MoveVariant, GeoCube};
 
 pub fn single_moves(c: &mut Criterion) {
     c.bench_function("All single moves", |b| b.iter(|| {
-        black_box(GeoCube::new().apply_move(Move::U(MoveVariant::Standard))
-                                .apply_move(Move::R(MoveVariant::Standard))
-                                .apply_move(Move::F(MoveVariant::Standard))
-                                .apply_move(Move::L(MoveVariant::Standard))
-                                .apply_move(Move::D(MoveVariant::Standard))
-                                .apply_move(Move::B(MoveVariant::Standard))
+        black_box(GeoCube::new(3).apply_move(Move::U(MoveVariant::Standard))
+                                 .apply_move(Move::R(MoveVariant::Standard))
+                                 .apply_move(Move::F(MoveVariant::Standard))
+                                 .apply_move(Move::L(MoveVariant::Standard))
+                                 .apply_move(Move::D(MoveVariant::Standard))
+                                 .apply_move(Move::B(MoveVariant::Standard))
         );
     }));
 }
 
 pub fn state(c: &mut Criterion) {
     c.bench_function("Obtaining state", |b| b.iter(|| {
-        black_box(GeoCube::new().get_state());
+        black_box(GeoCube::new(3).get_state());
     }));
 }
 
