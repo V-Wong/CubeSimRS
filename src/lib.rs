edbf2b2ea1a4ba134be74f166e56a5af1921c65d
Vincent Wong <vincent@vwong.dev>
2021-09-04 22:30:16 +1000

    Add crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! A simple Rubik's Cube simulator and solver.
+//! 
+//! # Quick Start Guide
+//! To quickly get a Rubik's Cube simulation running, we can follow
+//! the code sample below:
+//! 
+//! ```rust
+//! use cubesim::{Cube, FaceletCube, Move, MoveVariant};
+//! 
+//! let cube = FaceletCube::new(3);
+//! let rotated_cube = cube.apply_move(Move::U(MoveVariant::Double));
+//! println!("{}", rotated_cube.get_state());
+//! ```
+
 mod generic_cube;
 mod geometric_cube;
 mod facelet_cube;
