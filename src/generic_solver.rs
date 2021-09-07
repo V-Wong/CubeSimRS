139e24fe0a3e25d022bdbb69c813786da18835df
Vincent Wong <vincent@vwong.dev>
2021-09-07 21:15:19 +1000

    Add handling of unreachable solve states

--- src/generic_solver.rs
+++ src/generic_solver.rs
@@ -4,10 +4,15 @@ use crate::prelude::{Cube, Face, Move};
 pub trait Solver {
     /// Solves the Cube into the given solve state using the given moveset.
     /// 
+    /// If the solve state is reachable using the given moveset, the sequence
+    /// of moves will be returned as a ``Some(Vec<Move>)``.
+    /// 
+    /// If the solve state is not reachable using the given moveset, ``None`` is returned.
+    /// 
     /// # Arguments
     ///
     /// * `cube` - The Cube to be solved.
     /// * `moveset` - The moves to be used in the solution.
     /// * `solved_state` - The desired end state of a solution.
-    fn solve(cube: impl Cube, moveset: &[Move], solved_state: &[Face]) -> Vec<Move>;
+    fn solve(cube: impl Cube, moveset: &[Move], solved_state: &[Face]) -> Option<Vec<Move>>;
 }
