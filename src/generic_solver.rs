b1fe2176339089b4913f59f9655a1479ecb63ecb
Vincent Wong <vincent@vwong.dev>
2021-09-07 21:10:11 +1000

    Add documentation for generic solver

--- src/generic_solver.rs
+++ src/generic_solver.rs
@@ -1,5 +1,13 @@
 use crate::prelude::{Cube, Face, Move};
 
+/// A Rubik's Cube solver.
 pub trait Solver {
+    /// Solves the Cube into the given solve state using the given moveset.
+    /// 
+    /// # Arguments
+    ///
+    /// * `cube` - The Cube to be solved.
+    /// * `moveset` - The moves to be used in the solution.
+    /// * `solved_state` - The desired end state of a solution.
     fn solve(cube: impl Cube, moveset: &[Move], solved_state: &[Face]) -> Vec<Move>;
 }
