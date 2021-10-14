0a4b15a0b55c9725c419ee9ba1694f7bb25a2957
Vincent Wong <vincent@vwong.dev>
2021-10-14 23:31:10 +1100

    Add facelet cube benchmarks

---
+++ benches/facelet_cube.rs
@@ -0,0 +1,29 @@
+//
+// Facelet Cube Benchmarks
+//
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use cubesim::prelude::*;
+use cubesim::cube_implementors::{FaceletCube};
+
+pub fn single_moves(c: &mut Criterion) {
+    c.bench_function("All single moves", |b| b.iter(|| {
+        black_box(FaceletCube::new(3).apply_move(Move::U(MoveVariant::Standard))
+                                 .apply_move(Move::R(MoveVariant::Standard))
+                                 .apply_move(Move::F(MoveVariant::Standard))
+                                 .apply_move(Move::L(MoveVariant::Standard))
+                                 .apply_move(Move::D(MoveVariant::Standard))
+                                 .apply_move(Move::B(MoveVariant::Standard))
+        );
+    }));
+}
+
+pub fn state(c: &mut Criterion) {
+    c.bench_function("Obtaining state", |b| b.iter(|| {
+        black_box(FaceletCube::new(3).get_state());
+    }));
+}
+
+criterion_group!(benches, single_moves, state);
+criterion_main!(benches);
