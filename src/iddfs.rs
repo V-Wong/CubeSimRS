99866e4d114b820359803330d0c8eba79b055c4a
Vincent Wong <vincent@vwong.dev>
2021-10-15 23:03:59 +1100

    Add helper struct to hold solver data

--- src/iddfs.rs
+++ src/iddfs.rs
@@ -3,6 +3,36 @@ use std::mem::discriminant;
 
 use crate::generic_cube::{Cube, Face, Move};
 
+struct Solver {
+    candidate_moves: Vec<Move>,
+    pruning_table: HashMap<Vec<Face>, i32>,
+    pruning_depth: i32
+}
+
+impl Solver {
+    pub fn new(candidate_moves: Vec<Move>, pruning_table: HashMap<Vec<Face>, i32>, pruning_depth: i32) -> Self {
+        Self {
+            candidate_moves,
+            pruning_table,
+            pruning_depth
+        }
+    }
+
+    pub fn is_solved(&self, cube: &impl Cube) -> bool {
+        match self.pruning_table.get(&cube.get_state()) {
+            Some(0) => return true,
+            _ => false
+        }
+    }
+
+    pub fn lower_bound(&self, cube: &impl Cube) -> i32 {
+        match self.pruning_table.get(&cube.get_state()) {
+            Some(n) => *n,
+            _ => self.pruning_depth + 1
+        }
+    }
+}
+
 pub fn iddfs(cube: &impl Cube, 
              moveset: &[Move], 
              solved_state: &[Face],
