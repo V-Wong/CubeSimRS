4b789610475a7c3acbcb268b8cdb4b0daa1d02ec
Vincent Wong <vincent@vwong.dev>
2021-10-19 12:02:42 +1100

    Remove cube construction in benchmarks

--- benches/geometric_cube.rs
+++ benches/geometric_cube.rs
@@ -8,20 +8,24 @@ use cubesim::prelude::*;
 use cubesim::cube_implementors::{GeoCube};
 
 pub fn single_moves(c: &mut Criterion) {
+    let cube = GeoCube::new(3);
+
     c.bench_function("Geometric cube single moves", |b| b.iter(|| {
-        black_box(GeoCube::new(3).apply_move(Move::U(MoveVariant::Standard))
-                                 .apply_move(Move::R(MoveVariant::Standard))
-                                 .apply_move(Move::F(MoveVariant::Standard))
-                                 .apply_move(Move::L(MoveVariant::Standard))
-                                 .apply_move(Move::D(MoveVariant::Standard))
-                                 .apply_move(Move::B(MoveVariant::Standard))
+        black_box(cube.apply_move(Move::U(MoveVariant::Standard))
+                      .apply_move(Move::R(MoveVariant::Standard))
+                      .apply_move(Move::F(MoveVariant::Standard))
+                      .apply_move(Move::L(MoveVariant::Standard))
+                      .apply_move(Move::D(MoveVariant::Standard))
+                      .apply_move(Move::B(MoveVariant::Standard))
         );
     }));
 }
 
 pub fn state(c: &mut Criterion) {
+    let cube = GeoCube::new(3);
+
     c.bench_function("Geometric cube state", |b| b.iter(|| {
-        black_box(GeoCube::new(3).get_state());
+        black_box(cube.get_state());
     }));
 }
 
