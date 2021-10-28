26b57f5846cf8075289d27c4db0ffa00b4956122
Vincent Wong <vincent@vwong.dev>
2021-10-29 00:55:11 +1100

    Add depth first search implementation

--- src/generic_solver.rs
+++ src/generic_solver.rs
@@ -1,4 +1,6 @@
 use rustc_hash::FxHashMap;
+use std::mem::discriminant;
+
 use crate::prelude::{Cube, Face, Move};
 
 pub struct Solver {
@@ -29,4 +31,42 @@ impl Solver {
             _ => self.pruning_depth + 1
         }
     }
+}
+
+fn dfs(cube: &impl Cube, 
+       solver: &Solver, 
+       solution: &mut Vec<Move>,
+       depth_remaining: i32) -> Option<Vec<Move>> {
+    if solver.is_solved(cube) {
+        return Some(solution.to_vec());
+    }
+
+    if depth_remaining == 0 {
+        return None;
+    }
+
+    for mv in &solver.candidate_moves {
+        if let Some(last_mv) = solution.last() {
+            if discriminant(last_mv) == discriminant(mv) {
+                continue;
+            }
+        }
+
+        solution.push(*mv);
+
+        let result = dfs(
+            &cube.apply_move(*mv),
+            &solver,
+            solution,
+            depth_remaining - 1
+        );
+
+        solution.pop();
+
+        if result.is_some() {
+            return result;
+        }
+    }
+
+    None
 }
