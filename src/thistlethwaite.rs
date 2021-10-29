31fb096cd71b3df2aee8afcfe36748099422907f
Vincent Wong <vincent@vwong.dev>
2021-10-30 02:15:54 +1100

    Add phase 2 solver

--- src/thistlethwaite.rs
+++ src/thistlethwaite.rs
@@ -24,3 +24,43 @@ pub fn phase1(cube: &impl Cube) -> Option<Vec<Move>> {
     ida_star(&cube.mask(&mask), &solver, search_limit)
 }
 
+pub fn phase2(cube: &impl Cube) -> Option<Vec<Move>> {
+    use Face::*;
+    use MoveVariant::*;
+
+    let co_pieces = vec![
+        S(3, U, 1), S(3, U, 3), S(3, U, 7), S(3, U, 9),
+        S(3, D, 1), S(3, D, 3), S(3, D, 7), S(3, D, 9)
+    ];
+
+    let eo_ud_pieces = vec![
+        S(3, U, 2), S(3, U, 4), S(3, U, 6), S(3, U, 8),
+        S(3, D, 2), S(3, D, 4), S(3, D, 6), S(3, D, 8)
+    ];
+
+    let eo_e_piecs = vec![
+        S(3, F, 4), S(3, F, 6), S(3, B, 4), S(3, B, 6)
+    ];
+
+    let mask = |i: i32, _| 
+        if eo_ud_pieces.contains(&i) || co_pieces.contains(&i) { X } 
+        else { if eo_e_piecs.contains(&i) { U } else { R } };
+
+    let moves = vec![
+        Move::U(Standard), Move::U(Inverse), Move::U(Double), 
+        Move::D(Standard), Move::D(Inverse), Move::D(Double),  
+        Move::L(Standard), Move::L(Inverse), Move::L(Double), 
+        Move::R(Standard), Move::R(Inverse), Move::R(Double),  
+        Move::F(Double), Move::B(Double) 
+    ];
+
+    let search_limit = 10;
+    let pruning_depth = 5;
+    let pruning_table = gen_pruning_table(vec![FaceletCube::new(3).mask(&mask)], pruning_depth, &moves);
+
+    let solver = Solver::new(moves, pruning_table, pruning_depth);
+
+    ida_star(&cube.mask(&mask), &solver, search_limit)
+}
+
+
