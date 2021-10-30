01c07dcc5349cfb5d8780a91907000bc3726d2b7
Vincent Wong <vincent@vwong.dev>
2021-10-31 00:40:58 +1100

    Add basic scramble tests

---
+++ tests/thistlethwaite.rs
@@ -0,0 +1,28 @@
+//
+// Thistlethwaite Solver Tests
+//
+// We only test the Facelet Cube due to performance considerations
+//
+
+use cubesim::prelude::*;
+use cubesim::cube_implementors::{GeoCube, FaceletCube};
+use cubesim::solvers::solve;
+use cubesim::parse_scramble;
+
+#[test]
+fn solved_state() {
+    let cube = FaceletCube::new(3);
+    assert!(cube.apply_moves(&solve(&cube).unwrap()).is_solved());
+}
+
+#[test]
+fn single_move() {
+    let cube = FaceletCube::new(3).apply_move(Move::U(MoveVariant::Standard));
+    assert!(cube.apply_moves(&solve(&cube).unwrap()).is_solved());
+}
+
+#[test]
+fn longer_scramble() {
+    let cube = FaceletCube::new(3).apply_moves(&parse_scramble(String::from("U F R2 D2 B2 L R")));
+    assert!(cube.apply_moves(&solve(&cube).unwrap()).is_solved());
+}
