dc45731ddce907c79395b414364100fbd9d63ee1
Vincent Wong <vincent@vwong.dev>
2021-10-31 00:41:35 +1100

    Add superflip scramble test

--- tests/thistlethwaite.rs
+++ tests/thistlethwaite.rs
@@ -26,3 +26,12 @@ fn longer_scramble() {
     let cube = FaceletCube::new(3).apply_moves(&parse_scramble(String::from("U F R2 D2 B2 L R")));
     assert!(cube.apply_moves(&solve(&cube).unwrap()).is_solved());
 }
+
+#[test]
+fn superflip() {
+    let cube = FaceletCube::new(3).apply_moves(
+        &parse_scramble(String::from("U R2 F B R B2 R U2 L B2 R U' D' R2 F R' L B2 U2 F2"))
+    );
+    assert!(cube.apply_moves(&solve(&cube).unwrap()).is_solved());
+}
+
