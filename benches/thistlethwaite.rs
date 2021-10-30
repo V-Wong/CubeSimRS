6d5b9bad2c7b51e7703db252eaf8b63143768582
Vincent Wong <vincent@vwong.dev>
2021-10-31 01:10:14 +1100

    Add random scramble benchmark

--- benches/thistlethwaite.rs
+++ benches/thistlethwaite.rs
@@ -18,5 +18,14 @@ pub fn superflip(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(benches, superflip);
+pub fn random_scramble(c: &mut Criterion) {
+    let scramble = parse_scramble(String::from("D2 U2 R' U2 B2 L U2 L' D2 L' R' U2 F' L' B L B2 U' R"));
+    let cube = FaceletCube::new(3).apply_moves(&scramble);
+
+    c.bench_function("Random scramble", |b| b.iter(|| {
+        black_box(solve(&cube));
+    }));
+}
+
+criterion_group!(benches, superflip, random_scramble);
 criterion_main!(benches);
